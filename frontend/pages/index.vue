accc3d7246888cd5cab62c6ebddf162ca6454743
Marcos - gdotg.com <marcos@gdotg.com>
2018-07-02 17:36:45 -0300

    Cambios de estilo en la página principal

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,8 +1,21 @@
 <template>
   <v-layout row wrap>
+    <v-card-text style="height: 100px; position: relative">
     <div v-for="item in projects" :key="item.id">
       <ProjectTeaser :id="item.id" :image="item.thumbnail" :title="item.title" :description="item.description" />
     </div>
+    <v-btn
+    absolute
+    dark
+    fab
+    bottom
+    right
+    color="blue"
+    href="project/new"
+    >
+    <v-icon>add</v-icon>
+  </v-btn>
+  </v-card-text>
   </v-layout>
 </template>
 
