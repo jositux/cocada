f38e8d304e579a2bc70f5810f843324cec3c5074
Marcos - gdotg.com <marcos@gdotg.com>
2018-07-02 17:32:54 -0300

    Formulario nuevo proyecto

---
+++ frontend/pages/project/new.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-layout row wrap>
+        <v-card-text style="height: 100px; position: relative">
+            <h2>Nuevo Proyecto</h2>
+            <v-text-field 
+                id="nombre" 
+                name="nombre" 
+                label="Nombre" 
+            ></v-text-field>
+            <v-text-field
+                id="descripcion"
+                name="descripcion"
+                label="Descripción"
+                textarea
+            ></v-text-field>
+
+            <div><v-icon large color="blue darken-2">backup</v-icon></div>
+            <div>
+                <upload-button title="Examinar" :selectedCallback="fileSelectedFunc">
+            </upload-button></div>
+
+            <div class="actions">
+                <v-btn>Guardar</v-btn>
+            </div>
+        </v-card-text>
+    </v-layout>
+</template>
+<script>
+import UploadButton from '@/components//UploadButton';
+
+export default {
+    components: {
+      UploadButton
+    },
+  data() {
+    return {
+      project: [],
+      errors: []
+    }
+  },
+}
+</script>
