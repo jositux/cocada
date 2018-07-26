4c4c23734939f5fe579b33795eb02a25384f19fc
Marcos - gdotg.com <marcos@gdotg.com>
2018-07-26 19:41:25 -0300

    Página de logout

--- frontend/pages/user/logout.vue
+++ frontend/pages/user/logout.vue
@@ -1,7 +1,7 @@
 <template>
     <v-layout row wrap>
         <v-card-text style="height: 100px; position: relative">
-             <b-btn class="ml-3" variant="danger" @click="$auth.logout()">Logout</b-btn>
+             <v-btn class="ml-3" variant="danger" @click="$auth.logout()">Logout</v-btn>
         </v-card-text>
     </v-layout>
 </template>
