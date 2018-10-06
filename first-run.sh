ac4672ba8acc8a89601c133406bf524d0510e9f0
Marcos <marcos@gdotg.com>
2018-10-06 10:50:12 -0300

    first-run autoinstala las librerías de cypress

--- first-run.sh
+++ first-run.sh
@@ -43,6 +43,17 @@ else
     exit 1
 fi
 
+echo "Docker: Instalando librerías e2e (cypress.io), espere..."
+docker-compose run cypress npm install -q  > ./log/cypress_npm_install.log 2>&1
+if [ $? -eq 0 ]; then
+    echo "El log se guardo en ./log/cypress_npm_install.log"
+    echo "ok"
+else
+    echo "ERROR: El log se guardo en ./log/cypress_npm_install.log"
+    exit 1
+fi
+
+
 docker-compose rm -s --force  > /dev/null 2>&1
 docker-compose rm -s --force  > /dev/null 2>&1
 echo "****************************************************************"
