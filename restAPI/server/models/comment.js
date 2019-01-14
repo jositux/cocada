927fbdd0fb46d3590f19172b7dbdd0ad93186782
Marcos - gdotg.com <marcos@gdotg.com>
2019-01-14 09:51:40 -0300

    Versiones y comentarios (backend)

--- restAPI/server/models/comment.js
+++ restAPI/server/models/comment.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Comment) {
-
+    // calculated author property
+  Comment.observe('loaded', function(ctx, next) {
+    if (ctx.instance) {
+      var x = Comment.app.models.user.find({
+        where: {'id': ctx.instance.author_id},
+        limit: 1,
+      }, function(err, versions) {
+        if (err) return next(err);
+        if (versions.length) {
+          ctx.instance.author = versions.pop();
+        } else {
+          ctx.instance.author = {}; // this will never happen. See 'after save'
+        }
+        return next();
+      });
+    } else {
+      return next();
+    }
+  });
 };
