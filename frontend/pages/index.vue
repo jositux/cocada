b20261a9a3d1ba9f5fc8b7205538bfda0c50dedb
Marcos - gdotg.com <marcos@gdotg.com>
2019-08-01 19:33:14 -0300

    Estilo listado de proyectos

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,25 @@
 <template>
   <v-layout row wrap>
-    <v-card-text style="position: relative; padding-top: 0px;">
-      <v-btn dark fab absolute top right color="primary"
-      href="project/new"
-      title="Crear uno nuevo."
-      class="nuevo">
-        <v-icon>add</v-icon> 
-    </v-btn>
-    </v-card-text>
-    
-    <div v-for="item in projects" :key="item.id">
-      <ProjectTeaser :id="item.id" :image="item._lastVersion.image" :title="item.title" :description="item.description" />
-    </div>
+    <v-flex lg6 sm4 offset-sm3>
+      <v-container fluid grid-list-sm>
+        <v-card-text style="position: relative; padding-top: 0px;">
+          <v-btn dark fab absolute top right color="primary"
+          href="project/new"
+          title="Crear uno nuevo"
+          class="nuevo">
+            <v-icon>add</v-icon> 
+        </v-btn>
+        </v-card-text>
+        
+        <v-layout wrap>
+        <div v-for="item in projects" :key="item.id">
+          <ProjectTeaser :id="item.id" :image="item._lastVersion.image" :title="item.title" :description="item.description" />
+        </div>
+        </v-layout>
+      </v-container>
+    </v-flex>
   </v-layout>
+
 </template>
 
 <script>
